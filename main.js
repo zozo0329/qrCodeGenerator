ebf15f5f58cfd7bbe9d384e2be236c788e7abb27
Zo <161946731+zozo0329@users.noreply.github.com>
2024-11-05 22:28:01 +0800

    the enter button add

--- main.js
+++ main.js
@@ -2,14 +2,44 @@
     https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=
 */
 
-const qrButton = document
+// const qrButton = document
+//   .querySelector(".qr-btn button")
+//   .addEventListener("click", function () {
+//     const qrImage = document.querySelector(".qr-code img");
+//     const qrText = document.querySelector("input");
+//     const qrContainer = document.querySelector(".qr-code");
+//     qrContainer.style.display = "block";
+//     qrImage.src =
+//       "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+//       qrText.value;
+//     document
+//       .querySelector(".qr-btn button")
+//       .addEventListener("click", generateQRCode);
+//     document
+//       .querySelector("input")
+//       .addEventListener("keypress", function (event) {
+//         if (event.key === "Enter") {
+//           generateQRCode();
+//         }
+//       });
+//   });
+
+function generateQRCode() {
+  const qrImage = document.querySelector(".qr-code img");
+  const qrText = document.querySelector("input");
+  const qrContainer = document.querySelector(".qr-code");
+  qrContainer.style.display = "block";
+  qrImage.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+    qrText.value;
+}
+
+document
   .querySelector(".qr-btn button")
-  .addEventListener("click", function () {
-    const qrImage = document.querySelector(".qr-code img");
-    const qrText = document.querySelector("input");
-    const qrContainer = document.querySelector(".qr-code");
-    qrContainer.style.display = "block";
-    qrImage.src =
-      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-      qrText.value;
-  });
+  .addEventListener("click", generateQRCode);
+
+document.querySelector("input").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    generateQRCode();
+  }
+});
